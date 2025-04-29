d615298857a76c0044bb06c10d6fd178386fed2b
Pablo Alves <74788322+aquelepablo@users.noreply.github.com>
2025-04-29 15:42:57 -0300

    feat: solve Basic JS exercise – factorial function

--- 01-basic-js/script.js
+++ 01-basic-js/script.js
@@ -1,6 +1,7 @@
 const character = "#";
-const count = 8;
+const count = 10;
 const rows = [];
+let inverted = false;
 
 function padRow(rowNumber, rowCount) {
   return (
@@ -10,18 +11,13 @@ function padRow(rowNumber, rowCount) {
   );
 }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
-
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
-
-/*for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}*/
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
 
 let result = "";
 
