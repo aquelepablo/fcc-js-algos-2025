ccd070001504b3f8d07ebae93d717cba0dc85588
aquelePablo <74788322+aquelepablo@users.noreply.github.com>
2025-05-21 12:07:04 +0200

    chore: 62% complete

--- 05-calorie-counter-form/script.js
+++ 05-calorie-counter-form/script.js
@@ -21,8 +21,21 @@ function addEntry() {
     `#${entryDropdown.value} .input-container`
   );
   const entryNumber =
-    targetInputContainer.querySelectorAll('input[type="text"]').length;
+    targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
   const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />`;
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />
+  `;
+  targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
+}
+
+addEntryButton.addEventListener("click", addEntry);
+
+function getCaloriesFromInputs(list) {
+  let calories = 0;
+  for (const item of list) {
+    console.log(item);
+  }
 }
