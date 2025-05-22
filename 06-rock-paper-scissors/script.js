7d01a435ca0852bcf9b3d554d3995c962a7526ce
aquelePablo <74788322+aquelepablo@users.noreply.github.com>
2025-05-22 21:43:15 +0200

    feat(getRoundResults): Compute results of round

--- 06-rock-paper-scissors/script.js
+++ 06-rock-paper-scissors/script.js
@@ -3,6 +3,10 @@ function getRandomComputerResult() {
   const randomIndex = Math.floor(options.length * Math.random());
   return options[randomIndex];
 }
+
+let playerScore = 0;
+let computerScore = 0;
+
 function hasPlayerWonTheRound(player, computer) {
   return (
     (player === "Rock" && computer === "Scissors") ||
@@ -11,5 +15,18 @@ function hasPlayerWonTheRound(player, computer) {
   );
 }
 
-console.log(hasPlayerWonTheRound("Rock", "Scissors"));
-console.log(hasPlayerWonTheRound("Scissors", "Rock"));
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+  if (userOption === computerResult) {
+    return `It's a tie! Both chose ${userOption}`;
+  } else if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
+  }
+}
+
+console.log(getRoundResults("Rock"));
+console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
