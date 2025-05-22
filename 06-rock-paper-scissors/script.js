eb8aa09c911595d4313e41ef635acceb8ae8c088
aquelePablo <74788322+aquelepablo@users.noreply.github.com>
2025-05-22 21:22:08 +0200

    chore: 33% complete

--- 06-rock-paper-scissors/script.js
+++ 06-rock-paper-scissors/script.js
@@ -1,4 +1,19 @@
 function getRandomComputerResult() {
   const options = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(options.length * Math.random());
+  return options[randomIndex];
 }
-console.log(getRandomComputerResult());
+function hasPlayerWonTheRound(player, computer) {
+  let hasWon = false;
+  if (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  ) {
+    hasWon = true;
+  }
+  return hasWon;
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors"));
+console.log(hasPlayerWonTheRound("Scissors", "Rock"));
