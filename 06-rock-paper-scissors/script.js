bb2e0ca3644634f353592ad4089e5ee19f6eb66e
aquelePablo <74788322+aquelepablo@users.noreply.github.com>
2025-05-22 22:13:23 +0200

    feat(showResults): added 3 rounds validation

--- 06-rock-paper-scissors/script.js
+++ 06-rock-paper-scissors/script.js
@@ -31,11 +31,35 @@ function getRoundResults(userOption) {
 const playerScoreSpanElement = document.getElementById("player-score");
 const computerScoreSpanElement = document.getElementById("computer-score");
 const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
 
 function showResults(userOption) {
   roundResultsMsg.innerText = getRoundResults(userOption);
   playerScoreSpanElement.innerText = playerScore;
   computerScoreSpanElement.innerText = computerScore;
+
+  if (playerScore === 3 || computerScore === 3) {
+    const winner = playerScore === 3 ? "Player" : "Computer";
+    winnerMsgElement.innerText = `${winner} has won the game!`;
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
+  }
 }
 
-showResults("Rock");
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+
+rockBtn.addEventListener("click", function () {
+  showResults("Rock");
+});
+
+paperBtn.addEventListener("click", function () {
+  showResults("Paper");
+});
+
+scissorsBtn.addEventListener("click", function () {
+  showResults("Scissors");
+});
